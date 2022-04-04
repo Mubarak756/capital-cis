262442934872e240ebc7552a3fa0346b591def4c
Mubarak756 <88033348+Mubarak756@users.noreply.github.com>
2022-04-04 07:05:28 +0530

    github webhook changes

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github webhook ");
+        System.out.println( "github webhook changes ");
     }
 }
